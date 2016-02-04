83a2e709c1d96cb739907466ea7a1f70d3b402c5
Jørgen Loe Kvalberg <jorgen.kvalberg@gmail.com>
2016-02-04 12:03:58 +0100

    Add jorgkv-config

---
+++ jorgkv-config.sh
@@ -0,0 +1,15 @@
+USR_LOCAL="/usr/local"
+USR="/usr"
+SO_EXT=dylib
+EMBED=ON
+
+CONFIG="Unix Makefiles"
+BUILD_TYPE="Release"
+if ! [ -z "$1" ]; then
+    CONFIG="$1"
+fi
+
+cmake   -G "$CONFIG"  \
+        -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
+        -DCMAKE_INSTALL_PREFIX=/usr/local \
+        -DWITH_DRAWING=ON 
